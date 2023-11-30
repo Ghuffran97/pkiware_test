f1d5b27575d08864dd0ada102080f54a13d5aecc
MAS3k <mian90kakakhel0@gmail.com>
2023-11-30 14:45:34 -0500

    top bar done

--- src/layouts/DashboardLayout/index.js
+++ src/layouts/DashboardLayout/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import styled from 'styled-components';
-const { Header, Content, Footer, Sider } = Layout;
+import React from "react";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import styled from "styled-components";
 
+import TopBar from "../../components/Topbar";
+
+const { Content, Footer, Sider } = Layout;
 
 const Logo = styled.div`
   height: 32px;
@@ -20,70 +26,66 @@ const Logo = styled.div`
   }
 `;
 const App = () => {
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
-    return (
-        <Layout>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <Logo>
-                    <h4>
-                        PKIware
-                    </h4>
-                </Logo>
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    defaultSelectedKeys={['4']}
-                    items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-                        (icon, index) => ({
-                            key: String(index + 1),
-                            icon: React.createElement(icon),
-                            label: `nav ${index + 1}`,
-                        }),
-                    )}
-                />
-            </Sider>
-            <Layout>
-                <Header
-                    style={{
-                        padding: 0,
-                        background: colorBgContainer,
-                    }}
-                />
-                <Content
-                    style={{
-                        margin: '24px 16px 0',
-                    }}
-                >
-                    <div
-                        style={{
-                            padding: 24,
-                            minHeight: 360,
-                            background: colorBgContainer,
-                        }}
-                    >
-                        content
-                    </div>
-                </Content>
-                <Footer
-                    style={{
-                        textAlign: 'center',
-                    }}
-                >
-                    Ant Design ©2023 Created by Ant UED
-                </Footer>
-            </Layout>
-        </Layout>
-    );
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <Logo>
+          <h4>PKIware</h4>
+        </Logo>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={[
+            UserOutlined,
+            VideoCameraOutlined,
+            UploadOutlined,
+            UserOutlined,
+          ].map((icon, index) => ({
+            key: String(index + 1),
+            icon: React.createElement(icon),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Sider>
+      <Layout>
+        <TopBar background={colorBgContainer} />
+        <Content
+          style={{
+            margin: "24px 16px 0",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            content
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 };
 export default App;
