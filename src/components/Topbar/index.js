95218ccc4c74afdfa4ebd9aec6a52e2ac5cbfcef
MAS3k <mian90kakakhel0@gmail.com>
2023-11-30 12:03:14 -0500

    folder structure

---
+++ src/components/Topbar/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header } = Layout;
+
+const TopBar = ({ background }) => {
+  return <Header style={{ padding: 0, background: background }}>
+
+
+
+
+
+
+
+
+
+
+    
+  </Header>;
+};
+
+export default TopBar;
